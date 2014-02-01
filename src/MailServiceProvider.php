f4ab62cb6b191f6d37bc648b055c6c1f9a44a0ce
Jason Judge <jason.judge@consil.co.uk>
2014-02-01 13:39:03 +0000

    First commit

---
+++ src/MailServiceProvider.php
@@ -0,0 +1,60 @@
+<?php namespace Academe\LaravelDkim;
+
+/**
+ * Notes:
+ * I am not sure if this is the best way to do this.
+ * What we want to do, is change the namespace of the "Mailer" that is regisered. To do that,
+ * we replicate the entire core register method with no changes, except we are running it in
+ * a new namespace. Ideally I would want to run the core method but in the local namespace.
+ * Not sure if reflection can do this? Will investigate.
+ *
+ * This method does not support chaining of signing packages. We extend the core mail service
+ * to add the signing, and ignore the needs of other packages that may want to add their own
+ * signing to the messages at this level.
+ */
+
+use Illuminate\Mail\MailServiceProvider as CoreMailServiceProvider;
+
+class MailServiceProvider extends CoreMailServiceProvider {
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->registerSwiftMailer();
+
+		$this->app['mailer'] = $this->app->share(function($app)
+		{
+			// Once we have create the mailer instance, we will set a container instance
+			// on the mailer. This allows us to resolve mailer classes via containers
+			// for maximum testability on said classes instead of passing Closures.
+			$mailer = new Mailer($app['view'], $app['swift.mailer']);
+
+			$mailer->setLogger($app['log'])->setQueue($app['queue']);
+
+			$mailer->setContainer($app);
+
+			// If a "from" address is set, we will set it on the mailer so that all mail
+			// messages sent by the applications will utilize the same "from" address
+			// on each one, which makes the developer's life a lot more convenient.
+			$from = $app['config']['mail.from'];
+
+			if (is_array($from) and isset($from['address']))
+			{
+				$mailer->alwaysFrom($from['address'], $from['name']);
+			}
+
+			// Here we will determine if the mailer should be in "pretend" mode for this
+			// environment, which will simply write out e-mail to the logs instead of
+			// sending it over the web, which is useful for local dev enviornments.
+			$pretend = $app['config']->get('mail.pretend', false);
+
+			$mailer->pretend($pretend);
+
+			return $mailer;
+		});
+	}
+}
+
---
+++ src/Mailer.php
@@ -0,0 +1,55 @@
+<?php namespace Academe\LaravelDkim;
+
+use Illuminate\Mail\Message;
+use Illuminate\Mail\Mailer as CoreMailer;
+use Swift_SignedMessage;
+use Swift_Signers_DKIMSigner;
+use Config;
+
+class Mailer extends CoreMailer {
+	/**
+	 * Create a new message instance.
+	 *
+	 * @return \Illuminate\Mail\Message
+	 */
+	protected function createMessage()
+	{
+		// Get the DKIM selector.
+		$selector = Config::get('mail.dkim.selector');
+
+		// If we have a DKIM selector, then add the signing.
+		if (!empty($selector)) {
+			// Use the signed message with support for signing.
+			// So long as there is a selector we will do this, even if a private
+			// key is not set. This is handy if the application wants to add its
+			// own signatures later.
+			$message = new Message(new Swift_SignedMessage);
+
+			// Get the key and domain name.
+			// Ideally these would be specific to the selector, with the selector being
+			// chosen according to the domain of the sending address. At this stage we do
+			// not know what the final sending addressis going to be.
+			$private_key = Config::get('mail.dkim.private_key');
+			$domain_name = Config::get('mail.dkim.domain_name');
+
+			if (!empty($private_key) && !empty($domain_name)) {
+				// Do the DKIM signing.
+				$dkim_signer = new Swift_Signers_DKIMSigner($private_key, $domain_name, $selector);
+				$message->attachSigner($dkim_signer);
+			}
+		} else {
+			// Non-signed message.
+			$message = new Message(new Swift_Message);
+		}
+
+		// If a global from address has been specified we will set it on every message
+		// instances so the developer does not have to repeat themselves every time
+		// they create a new message. We will just go ahead and push the address.
+		if (isset($this->from['address']))
+		{
+			$message->from($this->from['address'], $this->from['name']);
+		}
+
+		return $message;
+	}
+}
